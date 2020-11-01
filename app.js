43d722936c1a366bd4b4f137661929aeaec61cb6
Thapelo <thapelomataboge18@gmail.com>
2020-11-01 22:18:21 +0200

    Did all the styling for animated menu

---
+++ app.js
@@ -0,0 +1,28 @@
+const set = document.querySelector('.set');
+const nav = document.querySelector('nav');
+const navItems = document.querySelectorAll('nav li');
+const close = document.querySelector('.close');
+const line = document.querySelector('.line');
+
+set.addEventListener('click', () => {
+  navItems.forEach((l, k) => {
+    l.style.transform = 'translateX(0)';
+    l.style.transitionDelay = `${k / 10}s`;
+    l.style.opacity = 1;
+  });
+  close.style.transform = 'translateY(0%) scale(1)';
+  line.style.transform = 'scale(1)';
+});
+
+close.addEventListener('click', () => {
+  navItems.forEach((l, k) => {
+    k -= 7;
+    k *= k;
+    k = Math.sqrt(k);
+    l.style.transform = 'translateX(150%)';
+    l.style.transitionDelay = `${k / 15}s`;
+    l.style.opacity = 0;
+  });
+  close.style.transform = 'translateY(-32vh) scale(0)';
+  line.style.transform = 'scale(0)';
+})
